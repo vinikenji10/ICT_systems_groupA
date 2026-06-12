259c1346f9f10bc6cfda5f3599ccf78d06369d97
Vinicius Yamada <viniciusyamada@Viniciuss-MacBook-Air.local>
2026-06-12 14:12:37 +0900

    Updated security related files to allow CI/CD with github

---
+++ sit-club-hub/app/firebase/config.ts
@@ -0,0 +1,22 @@
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore, initializeFirestore, persistentLocalCache, persistentMultipleTabManager, Firestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCTC4EC_6TsKOC0RfiAB9pwQcYq-ImF-yQ",
+  authDomain: "ict-systems-project-a.web.app",
+  projectId: "ict-systems-project-a",
+  storageBucket: "ict-systems-project-a.firebasestorage.app",
+  messagingSenderId: "747967613386",
+  appId: "1:747967613386:web:9c76a3009097c17de72733",
+  measurementId: "G-Z6QDZS9TGC"
+};
+
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+export { app, auth, db, storage };
---
+++ sit-club-hub/app/utils/constants.ts
@@ -0,0 +1,6 @@
+export const ADMIN_UIDS = [
+  "Bf8woNamDMQLurgvocihNPFUeup1", 
+  "fyprbf3sJ9eyvTXoaWXAIS4fueA3",
+  "pm1x1PThnRYMYwJQ1NAHDNGsuDJ2",
+  "LfMEKE7bB2eSAOoBIRRr7fzHDEf2"
+];
